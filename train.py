c5aac71d7963b918eb4839744472bfb6619d49bb
honey <honey@honeydeMac-mini.local>
2025-06-15 17:12:10 +0800

    FIX：V2.1-dynamic-position

--- train.py
+++ train.py
@@ -84,8 +84,8 @@ def train():
     print(f"✅ 模型已保存至: {config.MODEL_PATH}")
 
     # 【新增】保存训练时使用的特征列表
-    joblib.dump(feature_cols, 'model/feature_list.pkl')
-    print("✅ 特征列已保存至: model/feature_list.pkl")
+    joblib.dump(feature_cols, 'models/feature_list.pkl')
+    print("✅ 特征列已保存至: models/feature_list.pkl")
 
 if __name__ == '__main__':
     train()
