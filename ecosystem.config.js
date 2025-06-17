07cdff1fdfe87e089a76b0de414bd73cdb9d6330
honey <honey@honeydeMac-mini.local>
2025-06-17 14:24:55 +0800

    ADD:ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "quant_okx",
+      script: ".venv/bin/python",
+      args: "run/scheduler.py",
+      cwd: "/root/quant_sol_project",
+      env: {
+        PYTHONPATH: "/root/quant_sol_project"
+      }
+    }
+  ]
+}
